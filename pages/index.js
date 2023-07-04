6cc32f8b2c7a80d2bef793d8dc4fd5ddecb5f6fa
b2200356084 <b2200356084@cs.hacettepe.edu.tr>
2023-07-04 13:15:37 +0300

    first commit

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,284 @@
 
-
 export default function Home() {
   return (
-    <>
-      <div>Hello</div>
+      <>
 
-    </>
+          <meta charSet="UTF-8" />
+          <title>Retro Games</title>
+          <link rel="stylesheet" href="/styles/style.css" />
+          <table className="navbar">
+              <tbody>
+              <tr></tr>
+              </tbody>
+          </table>
+          <table className="upper">
+              <tbody>
+              <tr>
+                  <td>
+                      <img src="bitsody-logo.png" />
+                  </td>
+                  <td className="title">Retro Games</td>
+              </tr>
+              </tbody>
+          </table>
+          <table className="lower">
+              <tbody>
+              <tr>
+                  <td>
+                      <table className="category-table">
+                          <tbody>
+                          <tr>
+                              <th>Category</th>
+                          </tr>
+                          <tr>
+                              <td>Adventure</td>
+                          </tr>
+                          <tr>
+                              <td>Shooter</td>
+                          </tr>
+                          <tr>
+                              <td>Fighting</td>
+                          </tr>
+                          <tr>
+                              <td>Puzzle</td>
+                          </tr>
+                          <tr>
+                              <td>Racing</td>
+                          </tr>
+                          <tr>
+                              <td>Sports</td>
+                          </tr>
+                          </tbody>
+                      </table>
+                  </td>
+                  <td>
+                      <table className="games-table">
+                          <tbody>
+                          <tr>
+                              <th>Games</th>
+                              <th>Name</th>
+                              <th>Release Date</th>
+                              <th>Rate</th>
+                              <th>Link</th>
+                          </tr>
+                          <tr>
+                              <td className="game-picture">
+                                  <img src="game1.jpeg" />
+                              </td>
+                              <td>Valorant</td>
+                              <td>20.06.2020</td>
+                              <td>4.5</td>
+                              <td className="download-link">
+                                  <a href="your-file-path" className="download-button">
+                                      Download
+                                  </a>
+                              </td>
+                          </tr>
+                          <tr>
+                              <td className="game-picture">
+                                  <img src="game2.jpeg" />
+                              </td>
+                              <td>Valorant</td>
+                              <td>20.06.2020</td>
+                              <td>4.5</td>
+                              <td className="download-link">
+                                  <a href="your-file-path" className="download-button">
+                                      Download
+                                  </a>
+                              </td>
+                          </tr>
+                          <tr>
+                              <td className="game-picture">
+                                  <img src="game3.jpeg" />
+                              </td>
+                              <td>Valorant</td>
+                              <td>20.06.2020</td>
+                              <td>4.5</td>
+                              <td className="download-link">
+                                  <a href="your-file-path" className="download-button">
+                                      Download
+                                  </a>
+                              </td>
+                          </tr>
+                          <tr>
+                              <td className="game-picture">
+                                  <img src="game4.jpeg" />
+                              </td>
+                              <td>Valorant</td>
+                              <td>20.06.2020</td>
+                              <td>4.5</td>
+                              <td className="download-link">
+                                  <a href="your-file-path" className="download-button">
+                                      Download
+                                  </a>
+                              </td>
+                          </tr>
+                          <tr>
+                              <td className="game-picture">
+                                  <img src="game5.jpeg" />
+                              </td>
+                              <td>Valorant</td>
+                              <td>20.06.2020</td>
+                              <td>4.5</td>
+                              <td className="download-link">
+                                  <a href="your-file-path" className="download-button">
+                                      Download
+                                  </a>
+                              </td>
+                          </tr>
+                          <tr>
+                              <td className="game-picture">
+                                  <img src="game6.jpeg" />
+                              </td>
+                              <td>Valorant</td>
+                              <td>20.06.2020</td>
+                              <td>4.5</td>
+                              <td className="download-link">
+                                  <a href="your-file-path" className="download-button">
+                                      Download
+                                  </a>
+                              </td>
+                          </tr><tr>
+                              <td className="game-picture">
+                                  <img src="game6.jpeg" />
+                              </td>
+                              <td>Valorant</td>
+                              <td>20.06.2020</td>
+                              <td>4.5</td>
+                              <td className="download-link">
+                                  <a href="your-file-path" className="download-button">
+                                      Download
+                                  </a>
+                              </td>
+                          </tr><tr>
+                              <td className="game-picture">
+                                  <img src="game6.jpeg" />
+                              </td>
+                              <td>Valorant</td>
+                              <td>20.06.2020</td>
+                              <td>4.5</td>
+                              <td className="download-link">
+                                  <a href="your-file-path" className="download-button">
+                                      Download
+                                  </a>
+                              </td>
+                          </tr><tr>
+                              <td className="game-picture">
+                                  <img src="game6.jpeg" />
+                              </td>
+                              <td>Valorant</td>
+                              <td>20.06.2020</td>
+                              <td>4.5</td>
+                              <td className="download-link">
+                                  <a href="your-file-path" className="download-button">
+                                      Download
+                                  </a>
+                              </td>
+                          </tr><tr>
+                              <td className="game-picture">
+                                  <img src="game6.jpeg" />
+                              </td>
+                              <td>Valorant</td>
+                              <td>20.06.2020</td>
+                              <td>4.5</td>
+                              <td className="download-link">
+                                  <a href="your-file-path" className="download-button">
+                                      Download
+                                  </a>
+                              </td>
+                          </tr><tr>
+                              <td className="game-picture">
+                                  <img src="game6.jpeg" />
+                              </td>
+                              <td>Valorant</td>
+                              <td>20.06.2020</td>
+                              <td>4.5</td>
+                              <td className="download-link">
+                                  <a href="your-file-path" className="download-button">
+                                      Download
+                                  </a>
+                              </td>
+                          </tr><tr>
+                              <td className="game-picture">
+                                  <img src="game6.jpeg" />
+                              </td>
+                              <td>Valorant</td>
+                              <td>20.06.2020</td>
+                              <td>4.5</td>
+                              <td className="download-link">
+                                  <a href="your-file-path" className="download-button">
+                                      Download
+                                  </a>
+                              </td>
+                          </tr><tr>
+                              <td className="game-picture">
+                                  <img src="game6.jpeg" />
+                              </td>
+                              <td>Valorant</td>
+                              <td>20.06.2020</td>
+                              <td>4.5</td>
+                              <td className="download-link">
+                                  <a href="your-file-path" className="download-button">
+                                      Download
+                                  </a>
+                              </td>
+                          </tr><tr>
+                              <td className="game-picture">
+                                  <img src="game6.jpeg" />
+                              </td>
+                              <td>Valorant</td>
+                              <td>20.06.2020</td>
+                              <td>4.5</td>
+                              <td className="download-link">
+                                  <a href="your-file-path" className="download-button">
+                                      Download
+                                  </a>
+                              </td>
+                          </tr><tr>
+                              <td className="game-picture">
+                                  <img src="game6.jpeg" />
+                              </td>
+                              <td>Valorant</td>
+                              <td>20.06.2020</td>
+                              <td>4.5</td>
+                              <td className="download-link">
+                                  <a href="your-file-path" className="download-button">
+                                      Download
+                                  </a>
+                              </td>
+                          </tr><tr>
+                              <td className="game-picture">
+                                  <img src="game6.jpeg" />
+                              </td>
+                              <td>Valorant</td>
+                              <td>20.06.2020</td>
+                              <td>4.5</td>
+                              <td className="download-link">
+                                  <a href="your-file-path" className="download-button">
+                                      Download
+                                  </a>
+                              </td>
+                          </tr>
+                          <tr>
+                              <td className="game-picture">
+                                  <img src="game7.jpeg" />
+                              </td>
+                              <td>Valorant</td>
+                              <td>20.06.2020</td>
+                              <td>4.5</td>
+                              <td className="download-link">
+                                  <a href="your-file-path" className="download-button">
+                                      Download
+                                  </a>
+                              </td>
+                          </tr>
+                          </tbody>
+                      </table>
+                  </td>
+              </tr>
+              </tbody>
+          </table>
+      </>
+
   )
 }
