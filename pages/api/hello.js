24642697570a187d696e7a5ddfe98150b6bf02f5
b2200356084 <b2200356084@cs.hacettepe.edu.tr>
2023-07-06 16:51:51 +0300

    connect with azureTable

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,10 +1,8 @@
 import {AzureNamedKeyCredential, odata, TableClient, TableServiceClient} from "@azure/data-tables";
 
 export default async function handler(req, res) {
-  console.log("osman");
   const response = await  createTableAndEntity()
   res.status(200).json(response);
-  console.log("balta");
 }
 const account = "retrogamesstorage";
 const accountKey = "IQO22MPzKrK8OgfK/L7Z4kFxl3LzoVQxcuScqZ+bTw0ALrFLD/uFP35ftCGR/+LEHIURjFMot8iQ+AStQfROJQ==";
